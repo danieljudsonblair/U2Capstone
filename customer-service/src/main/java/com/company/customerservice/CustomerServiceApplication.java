6bf8352234baf244e65cee52beefdcb706441851
s1191 <danieljudsonblair@gmail.com>
2019-08-19 20:57:29 -0400

    initial commit customer service

---
+++ customer-service/src/main/java/com/company/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,19 @@
+package com.company.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableCaching
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+}
