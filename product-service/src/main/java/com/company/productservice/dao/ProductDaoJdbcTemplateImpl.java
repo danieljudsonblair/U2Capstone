9e4dc387e01ed37c870c3d693620c6c2d2e83df7
s1191 <danieljudsonblair@gmail.com>
2019-08-21 11:09:56 -0400

    product dao layer complete

--- product-service/src/main/java/com/company/productservice/dao/ProductDaoJdbcTemplateImpl.java
+++ product-service/src/main/java/com/company/productservice/dao/ProductDaoJdbcTemplateImpl.java
@@ -2,12 +2,16 @@ package com.company.productservice.dao;
 
 import com.company.productservice.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+@Repository
 public class ProductDaoJdbcTemplateImpl implements ProductDao {
     private JdbcTemplate jdbcTemplate;
 
@@ -24,29 +28,55 @@ public class ProductDaoJdbcTemplateImpl implements ProductDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    @Transactional
     @Override
     public Product createProduct(Product product) {
-        return null;
+        jdbcTemplate.update(
+                INSERT_PRODUCT,
+                product.getProductName(),
+                product.getProductDescription(),
+                product.getListPrice(),
+                product.getUnitCost()
+        );
+
+        int id = jdbcTemplate.queryForObject("select LAST_INSERT_ID()", Integer.class);
+
+        product.setProductId(id);
+
+        return product;
     }
 
     @Override
     public Product getProduct(int productId) {
-        return null;
+        try {
+            return jdbcTemplate.queryForObject(SELECT_PRODUCT, this::mapRowToProduct, productId);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     @Override
     public List<Product> getAllProducts() {
-        return null;
+        return jdbcTemplate.query(SELECT_ALL_PRODUCTS, this::mapRowToProduct);
     }
 
+    @Transactional
     @Override
     public void updateProduct(Product product) {
-
+        jdbcTemplate.update(
+                UPDATE_PRODUCT,
+                product.getProductName(),
+                product.getProductDescription(),
+                product.getListPrice(),
+                product.getUnitCost(),
+                product.getProductId()
+        );
     }
 
+    @Transactional
     @Override
     public void deleteProduct(int productId) {
-
+        jdbcTemplate.update(DELETE_PRODUCT, productId);
     }
 
     private Product mapRowToProduct(ResultSet rs, int rowNum) throws SQLException {
@@ -55,7 +85,7 @@ public class ProductDaoJdbcTemplateImpl implements ProductDao {
         product.setProductName(rs.getString("product_name"));
         product.setProductDescription(rs.getString("product_description"));
         product.setListPrice(rs.getBigDecimal("list_price"));
-        product.setUnitCost(rs.getBigDecimal("unit_price"));
+        product.setUnitCost(rs.getBigDecimal("unit_cost"));
         return product;
     }
 }
--- product-service/src/test/java/com/company/productservice/daoTest/productDaoJdbcTemplateImplTest.java
+++ product-service/src/test/java/com/company/productservice/daoTest/productDaoJdbcTemplateImplTest.java
@@ -1,11 +1,22 @@
 package com.company.productservice.daoTest;
 
+import com.company.productservice.dao.ProductDao;
+import com.company.productservice.model.Product;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.math.BigDecimal;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class productDaoJdbcTemplateImplTest {
 
     @Autowired
@@ -24,15 +35,15 @@ public class productDaoJdbcTemplateImplTest {
         Product product = new Product();
         product.setProductName("Playstation");
         product.setProductDescription("Game console");
-        product.setListPrice(new BigDecimal(350.00));
-        product.setUnitCost(new BigDecimal(200.00));
+        product.setListPrice(new BigDecimal("350.00"));
+        product.setUnitCost(new BigDecimal("200.00"));
 
-        productDao.createProduct(product);
+        product = productDao.createProduct(product);
 
         Product product1 = productDao.getProduct(product.getProductId());
         assertEquals(product, product1);
 
-        productDao.deleteProduct(product.getProductId);
+        productDao.deleteProduct(product.getProductId());
         product1 = productDao.getProduct(product.getProductId());
         assertNull(product1);
     }
@@ -42,15 +53,15 @@ public class productDaoJdbcTemplateImplTest {
         Product product = new Product();
         product.setProductName("Playstation");
         product.setProductDescription("Game console");
-        product.setListPrice(new BigDecimal(350.00));
-        product.setUnitCost(new BigDecimal(200.00));
+        product.setListPrice(new BigDecimal("350.00"));
+        product.setUnitCost(new BigDecimal("200.00"));
 
         productDao.createProduct(product);
 
         product.setProductName("Playstation2");
         product.setProductDescription("Game console");
-        product.setListPrice(new BigDecimal(350.00));
-        product.setUnitCost(new BigDecimal(250.00));
+        product.setListPrice(new BigDecimal("350.00"));
+        product.setUnitCost(new BigDecimal("250.00"));
 
         productDao.updateProduct(product);
 
@@ -63,16 +74,16 @@ public class productDaoJdbcTemplateImplTest {
         Product product = new Product();
         product.setProductName("Playstation");
         product.setProductDescription("Game console");
-        product.setListPrice(new BigDecimal(350.00));
-        product.setUnitCost(new BigDecimal(200.00));
+        product.setListPrice(new BigDecimal("350.00"));
+        product.setUnitCost(new BigDecimal("200.00"));
 
         productDao.createProduct(product);
 
         Product product1 = new Product();
         product1.setProductName("Playstation2");
         product1.setProductDescription("Game console");
-        product1.setListPrice(new BigDecimal(350.00));
-        product1.setUnitCost(new BigDecimal(250.00));
+        product1.setListPrice(new BigDecimal("350.00"));
+        product1.setUnitCost(new BigDecimal("250.00"));
 
         productDao.createProduct(product1);
 
