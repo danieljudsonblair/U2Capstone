0d704bfdb2c23339fadd7acc8260e4ade37691a5
Darrell R <reeves.darrell@gmail.com>
2019-08-23 10:48:47 -0400

    updating retail service

--- retail-api-service/src/main/java/com/company/retailapiservice/util/feign/LevelUpClient.java
+++ retail-api-service/src/main/java/com/company/retailapiservice/util/feign/LevelUpClient.java
@@ -2,6 +2,7 @@ package com.company.retailapiservice.util.feign;
 
 import com.company.retailapiservice.model.LevelUp;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -9,8 +10,18 @@ import java.util.List;
 
 @FeignClient(name = "level-up-service")
 public interface LevelUpClient {
-    @GetMapping(value = "/levelup/customer/{customerId}")
-    public List<LevelUp> fetchLevelUpPointByCustomerId(@PathVariable int customerId);
+    @GetMapping("/levleups/{id}")
+    public LevelUp getLevelUpById(@PathVariable int id);
+
+    @GetMapping("/levelups")
+    public List<LevelUp> getAllLevelUps();
+
+    @GetMapping("/levelups/customer/{id}")
+    public List<LevelUp> getLevelUpsByCustomerId(@PathVariable int id);
+
+    @DeleteMapping("/levelups/{id}")
+    public void deleteLevelUp(@PathVariable int id);
+
 
 
 
---
+++ retail-api-service/src/main/java/com/company/retailapiservice/viewModel/InvoiceViewModel.java
@@ -0,0 +1,34 @@
+package com.company.retailapiservice.viewModel;
+
+import com.company.retailapiservice.model.Invoice;
+import com.company.retailapiservice.model.InvoiceItem;
+
+import java.util.List;
+import java.util.Objects;
+
+public class InvoiceViewModel extends Invoice {
+    private List<InvoiceItem> invoiceItemList;
+
+    public List<InvoiceItem> getInvoiceItemList() {
+        return invoiceItemList;
+    }
+
+    public void setInvoiceItemList(List<InvoiceItem> invoiceItemList) {
+        this.invoiceItemList = invoiceItemList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        InvoiceViewModel that = (InvoiceViewModel) o;
+        return getInvoiceItemList().equals(that.getInvoiceItemList());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getInvoiceItemList());
+    }
+}
+
