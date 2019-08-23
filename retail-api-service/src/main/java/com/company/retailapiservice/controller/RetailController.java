b4df8a5d259a644776adcafa7a6972347adb9692
s1191 <danieljudsonblair@gmail.com>
2019-08-23 14:01:22 -0400

    added purchaseViewModel to servicelayer

--- retail-api-service/src/main/java/com/company/retailapiservice/controller/RetailController.java
+++ retail-api-service/src/main/java/com/company/retailapiservice/controller/RetailController.java
@@ -6,6 +6,8 @@ import com.company.retailapiservice.model.InvoiceView;
 import com.company.retailapiservice.model.LevelUp;
 import com.company.retailapiservice.model.Product;
 import com.company.retailapiservice.service.ServiceLayer;
+import com.company.retailapiservice.viewModel.PurchaseReturnViewModel;
+import com.company.retailapiservice.viewModel.PurchaseSendViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
@@ -14,59 +16,59 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-    @RefreshScope
-    public class RetailController {
-        @Autowired
-        ServiceLayer service;
+@RefreshScope
+public class RetailController {
+    @Autowired
+    ServiceLayer service;
 
-        @PostMapping(value = "/invoices")
-        public InvoiceView createInvoice(@RequestBody @Valid InvoiceView invoiceView){
-            return service.createInvoice(invoiceView);
-        }
-
-        @GetMapping(value = "/invoices")
-        public List<InvoiceView> fetchAllInvoices(){
-            return service.getAllInvoices();
-        }
-
-        @GetMapping(value = "/invoices/{invoiceId}")
-        public InvoiceView fetchInvoicesById(@PathVariable int invoiceId){
-            return service.getInvoiceByInvoiceId(invoiceId);
-        }
-
-        @GetMapping(value = "/invoices/customer/{customerId}")
-        public List<InvoiceView> fetchInvoicesByCustomerId(@PathVariable int customerId){
-            return service.getInvoicesByCustomerId(customerId);
-        }
-
-        @GetMapping(value = "/products/{productId}")
-        public Product fetchProduct(@PathVariable int productId){
-            return service.getProductByProductId(productId);
-        }
-
-        @GetMapping(value = "/products/inventory")
-        public List<Product> fetchProductsCurrentlyInInventory(){
-            return service.getProductsInInventory();
-        }
-
-        @GetMapping(value = "/products/invoice/{invoiceId}")
-        public List<Product> fetchProductsByInvoiceId(@PathVariable int invoiceId){
-            return service.getProductsByInvoiceId(invoiceId);
-        }
-
-        @GetMapping(value = "/levelup/customer/{customerId}")
-        public List<LevelUp> fetchLevelUpPointByCustomerId(@PathVariable int customerId){
-            return service.getLevelUpPointsByCustomerId(customerId);
-        }
-
-
-        @GetMapping(value = "/inventory")
-        public List<Inventory> fetchAllInventories(){
-            return service.getAllInventories();
-        }
-
-        @GetMapping(value = "/inventory/{inventoryId}")
-        public Inventory fetchInventoryById(@PathVariable int inventoryId){
-            return service.getInventoryById(inventoryId);
-        }
+    @PostMapping(value = "/purchase")
+    public PurchaseReturnViewModel createPurchase(@RequestBody @Valid PurchaseSendViewModel psvm) {
+        return service.savePurchase(psvm);
     }
+
+    @GetMapping(value = "/invoices")
+    public List<InvoiceView> fetchAllInvoices() {
+        return service.getAllInvoices();
+    }
+
+    @GetMapping(value = "/invoices/{invoiceId}")
+    public InvoiceView fetchInvoicesById(@PathVariable int invoiceId) {
+        return service.getInvoiceByInvoiceId(invoiceId);
+    }
+
+    @GetMapping(value = "/invoices/customer/{customerId}")
+    public List<InvoiceView> fetchInvoicesByCustomerId(@PathVariable int customerId) {
+        return service.getInvoicesByCustomerId(customerId);
+    }
+
+    @GetMapping(value = "/products/{productId}")
+    public Product fetchProduct(@PathVariable int productId) {
+        return service.getProductByProductId(productId);
+    }
+
+    @GetMapping(value = "/products/inventory")
+    public List<Product> fetchProductsCurrentlyInInventory() {
+        return service.getProductsInInventory();
+    }
+
+    @GetMapping(value = "/products/invoice/{invoiceId}")
+    public List<Product> fetchProductsByInvoiceId(@PathVariable int invoiceId) {
+        return service.getProductsByInvoiceId(invoiceId);
+    }
+
+    @GetMapping(value = "/levelup/customer/{customerId}")
+    public List<LevelUp> fetchLevelUpPointByCustomerId(@PathVariable int customerId) {
+        return service.getLevelUpPointsByCustomerId(customerId);
+    }
+
+
+    @GetMapping(value = "/inventory")
+    public List<Inventory> fetchAllInventories() {
+        return service.getAllInventories();
+    }
+
+    @GetMapping(value = "/inventory/{inventoryId}")
+    public Inventory fetchInventoryById(@PathVariable int inventoryId) {
+        return service.getInventoryById(inventoryId);
+    }
+}
--- retail-api-service/src/main/java/com/company/retailapiservice/service/ServiceLayer.java
+++ retail-api-service/src/main/java/com/company/retailapiservice/service/ServiceLayer.java
@@ -2,18 +2,22 @@ package com.company.retailapiservice.service;
 
 import com.company.retailapiservice.exception.NotFoundException;
 import com.company.retailapiservice.model.*;
-import com.company.retailapiservice.util.feign.InventoryClient;
-import com.company.retailapiservice.util.feign.InvoiceClient;
-import com.company.retailapiservice.util.feign.LevelUpClient;
-import com.company.retailapiservice.util.feign.ProductClient;
+import com.company.retailapiservice.util.feign.*;
+import com.company.retailapiservice.viewModel.InventoryView;
+import com.company.retailapiservice.viewModel.ProductView;
+import com.company.retailapiservice.viewModel.PurchaseReturnViewModel;
+import com.company.retailapiservice.viewModel.PurchaseSendViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class ServiceLayer {
+    CustomerClient customerClient;
     InvoiceClient invoiceClient;
     ProductClient productClient;
     InventoryClient inventoryClient;
@@ -21,13 +25,28 @@ public class ServiceLayer {
     LevelUpProducer levelUpProducer;
 
     @Autowired
-    public ServiceLayer(InvoiceClient invoiceClient, ProductClient productClient, InventoryClient inventoryClient, LevelUpClient levelUpClient, LevelUpProducer levelUpProducer) {
+    public ServiceLayer(CustomerClient customerClient, InvoiceClient invoiceClient, ProductClient productClient, InventoryClient inventoryClient, LevelUpClient levelUpClient, LevelUpProducer levelUpProducer) {
+        this.customerClient = customerClient;
         this.invoiceClient = invoiceClient;
         this.productClient = productClient;
         this.inventoryClient = inventoryClient;
         this.levelUpClient = levelUpClient;
         this.levelUpProducer = levelUpProducer;
     }
+
+    private BigDecimal min = new BigDecimal("0");
+    private BigDecimal max = new BigDecimal("99999.99");
+
+    private String emptyField(String fieldName) {
+        return "Field name " + fieldName + " cannot be an empty string";
+    }
+
+    private String notFound(String objectName, int id) {
+        return "No " + objectName + " exists @ ID " + id;
+    }
+
+    @Autowired
+
 
     public InvoiceView createInvoice(InvoiceView invoiceView){
         return invoiceClient.createInvoice(invoiceView);
@@ -95,6 +114,102 @@ public class ServiceLayer {
         return inventoryClient.getInventoryById(inventoryId);
     }
 
+    private PurchaseReturnViewModel buildPurchaseReturnViewModel(PurchaseSendViewModel psvm) {
+        PurchaseReturnViewModel prvm = new PurchaseReturnViewModel();
+        List<ProductView> pvList = new ArrayList<>();
+        List<InvoiceItem> iiList = new ArrayList<>();
+        List<InventoryView> ivList;
+        BigDecimal invoiceTotalPrice = new BigDecimal("0");
+        Inventory clientInventory;
+        Product clientProduct;
+        Inventory updatedInventory = new Inventory();
 
+
+        if (psvm.getCustomer() == null) {
+            Customer customer = customerClient.getCustomer(psvm.getCustomerId());
+            if (customer == null)
+                throw new NotFoundException(notFound("customer", psvm.getCustomerId()));
+            prvm.setCustomer(customer);
+        } else if (psvm.getCustomer() != null && psvm.getCustomerId() == 0) {
+            prvm.setCustomer(customerClient.createCustomer(psvm.getCustomer()));
+        }
+
+        ivList = psvm.getInventoryList();
+        for (InventoryView i : ivList) {
+            InvoiceItem ii = new InvoiceItem();
+            ProductView pv = new ProductView();
+            clientInventory = inventoryClient.getInventoryById(i.getInventoryId());
+            clientProduct = productClient.getProductById(clientInventory.getProductId());
+
+
+            if (clientInventory == null)
+                throw new NotFoundException(notFound("inventory", i.getInventoryId()));
+            if (clientInventory.getQuantity() < i.getQuantity())
+                throw new IllegalArgumentException("You must select a lower quantity");
+
+            updatedInventory.setInventoryId(i.getInventoryId());
+            updatedInventory.setProductId(clientInventory.getProductId());
+            updatedInventory.setQuantity(clientInventory.getQuantity() - i.getQuantity());
+
+            inventoryClient.updateInventory(updatedInventory);
+            ii.setInventoryId(i.getInventoryId());
+
+            pv.setListPrice(clientProduct.getListPrice());
+            pv.setQuantity(BigDecimal.valueOf(i.getQuantity()));
+            pv.setProductTotal(pv.getQuantity().multiply(pv.getListPrice()).setScale(2, BigDecimal.ROUND_HALF_UP));
+            pv.setProductName(clientProduct.getProductName());
+            pv.setProductDescription(clientProduct.getProductDescription());
+            ii.setUnitPrice(pv.getListPrice());
+            ii.setQuantity(pv.getQuantity().intValue());
+            iiList.add(ii);
+
+            pvList.add(pv);
+        }
+        prvm.setProductList(pvList);
+
+        for (ProductView p : pvList) {
+            invoiceTotalPrice = invoiceTotalPrice.add(p.getProductTotal());
+        }
+        prvm.setTotalPrice(invoiceTotalPrice);
+        List<LevelUp> clientLevelUpList = new ArrayList<>();
+        LevelUp newLevelUp = new LevelUp();
+        LevelUp nextLevelUp = new LevelUp();
+        boolean newCustomer = false;
+        int levelUp = invoiceTotalPrice.divide(new BigDecimal("50")).setScale(1, BigDecimal.ROUND_FLOOR).intValue() * 10;
+
+        try {
+            clientLevelUpList = levelUpClient.getLevelUpsByCustomerId(prvm.getCustomer().getCustomerId());
+        } catch (Exception e) {
+            newLevelUp.setMemberDate(psvm.getPurchaseDate());
+            newCustomer = true;
+
+        }
+        if (!newCustomer && clientLevelUpList.get(0).getPoints() == -1) {
+            newLevelUp.setMemberDate(LocalDate.of(1999, 9, 9));
+        } else if (!newCustomer && clientLevelUpList.get(0).getPoints() != 1) {
+            newLevelUp.setMemberDate(clientLevelUpList.get(0).getMemberDate());
+        }
+        newLevelUp.setPoints(levelUp);
+        newLevelUp.setCustomerId(prvm.getCustomer().getCustomerId());
+        levelUpProducer.createLevelUp(newLevelUp);
+
+        prvm.setLvlUpPtsThisPurchase(levelUp);
+
+        InvoiceView iv = new InvoiceView();
+        Invoice invoice = new Invoice();
+
+        iv.setCustomerId(prvm.getCustomer().getCustomerId());
+        iv.setPurchaseDate(psvm.getPurchaseDate());
+        iv.setInvoiceItemList(iiList);
+
+        iv = invoiceClient.createInvoice(iv);
+        invoice.setPurchaseDate(iv.getPurchaseDate());
+        invoice.setCustomerId(prvm.getCustomer().getCustomerId());
+        invoice.setInvoiceId(iv.getInvoiceId());
+
+        prvm.setInvoice(invoice);
+
+        return prvm;
+    }
 
 }
